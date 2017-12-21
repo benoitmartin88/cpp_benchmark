3eba3d5ca1b3d694319b3a80e764f06ee0f680c9
Benoit Martin <benoit.martin@tredzone.com>
2017-12-21 15:05:14 +0100

    wip switch case

--- benchmark/switchcase.cpp
+++ benchmark/switchcase.cpp
@@ -7,75 +7,99 @@ enum TestEnum {
 };
 
 std::function<TestEnum()> r = [] {
-    return static_cast<TestEnum>(rand() % TEST3+1); // enum length is last element+1 (enum starts at 0)
+//    TestEnum e = static_cast<TestEnum>(rand() % TEST3+1); // enum length is last element+1 (enum starts at 0)
+    return TEST3;
 };
 
-static uint64_t single() {
-    switch(r()) {
-        case TEST1: return static_cast<uint64_t>(TEST1); break;
-        case TEST2: return static_cast<uint64_t>(TEST2); break;
-        case TEST3: return static_cast<uint64_t>(TEST3); break;
-        default: return 0;
+
+template<typename _T>
+static _T singleIf() {
+    auto i = r();
+    if(i != TEST1 && i != TEST2 && i != TEST3) {
+        return static_cast<_T>(TEST1);
     }
+
+    return rand();
 }
 
-static uint64_t multiple() {
+template<typename _T>
+static _T singleSwitch() {
     switch(r()) {
-        case TEST1: return static_cast<uint64_t>(TEST1); break;
-        case TEST2: return static_cast<uint64_t>(TEST2); break;
-        case TEST3: return static_cast<uint64_t>(TEST3); break;
+        case TEST1: return static_cast<_T>(TEST1); break;
+        case TEST2: return static_cast<_T>(TEST2); break;
+        case TEST3: break;
+        default: return 0;
+    }
+
+    return rand();
+}
+
+template<typename _T>
+static _T multipleSwitch() {
+    switch(r()) {
+        case TEST1: return static_cast<_T>(TEST1); break;
+        case TEST2: return static_cast<_T>(TEST2); break;
+        case TEST3: break;
         default: return 0;
     }
     switch(r()) {
-        case TEST1: return static_cast<uint64_t>(TEST1); break;
-        case TEST2: return static_cast<uint64_t>(TEST2); break;
-        case TEST3: return static_cast<uint64_t>(TEST3); break;
+        case TEST1: return static_cast<_T>(TEST1); break;
+        case TEST2: return static_cast<_T>(TEST2); break;
+        case TEST3: break;
         default: return 0;
     }
     switch(r()) {
-        case TEST1: return static_cast<uint64_t>(TEST1); break;
-        case TEST2: return static_cast<uint64_t>(TEST2); break;
-        case TEST3: return static_cast<uint64_t>(TEST3); break;
+        case TEST1: return static_cast<_T>(TEST1); break;
+        case TEST2: return static_cast<_T>(TEST2); break;
+        case TEST3: break;
         default: return 0;
     }
     switch(r()) {
-        case TEST1: return static_cast<uint64_t>(TEST1); break;
-        case TEST2: return static_cast<uint64_t>(TEST2); break;
-        case TEST3: return static_cast<uint64_t>(TEST3); break;
+        case TEST1: return static_cast<_T>(TEST1); break;
+        case TEST2: return static_cast<_T>(TEST2); break;
+        case TEST3: break;
         default: return 0;
     }
     switch(r()) {
-        case TEST1: return static_cast<uint64_t>(TEST1); break;
-        case TEST2: return static_cast<uint64_t>(TEST2); break;
-        case TEST3: return static_cast<uint64_t>(TEST3); break;
+        case TEST1: return static_cast<_T>(TEST1); break;
+        case TEST2: return static_cast<_T>(TEST2); break;
+        case TEST3: break;
         default: return 0;
     }
-    switch(r()) {
-        case TEST1: return static_cast<uint64_t>(TEST1); break;
-        case TEST2: return static_cast<uint64_t>(TEST2); break;
-        case TEST3: return static_cast<uint64_t>(TEST3); break;
-        default: return 0;
-    }
+
+    return rand();
 }
 
 
-static void singleSwitchCase(benchmark::State& state) {
-//    srand(time(NULL));
+static void singleSwitchCase8(benchmark::State& state) {
     for (auto _ : state) {
-        benchmark::DoNotOptimize(single());
+        benchmark::DoNotOptimize(singleSwitch<uint8_t>());
+    }
+}
+
+static void singleSwitchCase64(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(singleSwitch<uint64_t>());
+    }
+}
+
+static void singleIf64(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(singleIf<uint64_t>());
     }
 }
 
 static void multipleSwitchCase(benchmark::State& state) {
-//    srand(time(NULL));
     for (auto _ : state) {
-        benchmark::DoNotOptimize(multiple());
+        benchmark::DoNotOptimize(multipleSwitch<uint8_t>());
     }
 }
 
 
 
-BENCHMARK(singleSwitchCase);
+BENCHMARK(singleSwitchCase8);
+BENCHMARK(singleSwitchCase64);
+BENCHMARK(singleIf64);
 BENCHMARK(multipleSwitchCase);
 
 BENCHMARK_MAIN();
